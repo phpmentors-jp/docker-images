6e23efe758394b3e5ca7ddc94b563240fd2b9c68
KUBO Atsuhiro <kubo@iteman.jp>
2015-02-08 20:58:03 +0900

    [symfony-app] improve error handling for app-init.sh

--- symfony-app/php55/app/app-init.sh
+++ symfony-app/php55/app/app-init.sh
@@ -25,31 +25,109 @@ if [ ! -d "$application_dir" ]; then
 fi
 
 cd "$application_dir"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
 
 find "${application_dir}" ! -group $APACHE_RUN_GROUP -exec chgrp $APACHE_RUN_GROUP '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 find "${application_dir}" -type d ! -perm 2775 -exec chmod 2775 '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 find "${application_dir}" -type f ! -perm -g=w -exec chmod g+w '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
 
 if [ ! -e "${application_dir}/composer.phar" ]; then
     sudo -u $APACHE_RUN_USER php -r "readfile('https://getcomposer.org/installer');" | sudo -u $APACHE_RUN_USER php
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+        exit $RETVAL
+    fi
 fi
 
 if [ -e "${application_dir}/composer.phar" ]; then
     sudo -u $APACHE_RUN_USER COMPOSER_HOME="${application_dir}/.composer" php composer.phar self-update
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+        exit $RETVAL
+    fi
+
     sudo -u $APACHE_RUN_USER COMPOSER_HOME="${application_dir}/.composer" php composer.phar install --no-interaction
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+        exit $RETVAL
+    fi
 fi
 
 if [ -e "${application_dir}/composer.phar" ]; then
     sudo -u $APACHE_RUN_USER COMPOSER_HOME="${application_dir}/.composer" php composer.phar install --no-interaction
+    RETVAL=$?
+    if [ $RETVAL -ne 0 ]; then
+        exit $RETVAL
+    fi
 fi
 
 find "${application_dir}" ! -group $APACHE_RUN_GROUP -exec chgrp $APACHE_RUN_GROUP '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 find "${application_dir}" -type d ! -perm 2775 -exec chmod 2775 '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 find "${application_dir}" -type f ! -perm -g=w -exec chmod g+w '{}' \;
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
 
 sed -i "s/^\\( *database_driver:\\).*/\\1 ${database_driver}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 sed -i "s/^\\( *database_host:\\).*/\\1 ${database_host}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 sed -i "s/^\\( *database_port:\\).*/\\1 ${database_port}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 sed -i "s/^\\( *database_user:\\).*/\\1 ${database_user}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 sed -i "s/^\\( *database_password:\\).*/\\1 ${database_password}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
+
 sed -i "s/^\\( *secret:\\).*/\\1 ${secret}/" "$parameters_file"
+RETVAL=$?
+if [ $RETVAL -ne 0 ]; then
+    exit $RETVAL
+fi
