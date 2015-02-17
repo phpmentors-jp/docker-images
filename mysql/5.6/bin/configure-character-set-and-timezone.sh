517a2a7c216b6ea41da4667a161d5e4a76a8c993
KUBO Atsuhiro <kubo@iteman.jp>
2015-02-17 16:51:18 +0900

    [mysql] change code to configure the time zone always

--- mysql/5.6/bin/configure-character-set-and-timezone.sh
+++ mysql/5.6/bin/configure-character-set-and-timezone.sh
@@ -3,9 +3,9 @@ if [ -z "$(ls -A /var/lib/mysql)" -a "${1%_safe}" = 'mysqld' ]; then
         sed -i "s/^\\( *character_set_server\\) *= *.*/\\1 = ${MYSQL_CHARACTER_SET}/" /etc/mysql/conf.d/characterset.cnf
         sed -i "s/^\\( *default-character-set\\) *= *.*/\\1 = ${MYSQL_CHARACTER_SET}/" /etc/mysql/conf.d/characterset.cnf
     fi
+fi
 
-    if [ "x$TZ" != "x" ]; then
-        echo "$TZ" > /etc/timezone
-        dpkg-reconfigure -f noninteractive tzdata
-    fi
+if [ "x$TZ" != "x" ]; then
+    echo "$TZ" > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
 fi
